8e92ccf3cc42357cd9b4c9d7506fceabd33c48ef
loubnaaktar <loubnaakt13@gmail.com>
2026-03-30 16:55:42 +0100

    updates

--- src/main/java/org/example/fleetflow/FleetFlowApplication.java
+++ src/main/java/org/example/fleetflow/FleetFlowApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FleetFlowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FleetFlowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FleetFlowApplication.class, args);
+    }
 
 }
