16b8cb3f0e999ac1a80975c68762246f3d2462f6
loubnaaktar <loubnaakt13@gmail.com>
2026-04-02 10:16:46 +0100

    service et controller de chauffeures

--- src/main/java/org/example/fleetflow/controller/ChauffeurController.java
+++ src/main/java/org/example/fleetflow/controller/ChauffeurController.java
@@ -1,4 +1,38 @@
 package org.example.fleetflow.controller;
 
+import lombok.AllArgsConstructor;
+import org.example.fleetflow.DTO.ChauffeurDTO;
+import org.example.fleetflow.model.Chauffeur;
+import org.example.fleetflow.service.ChauffeurService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/chauffeures")
+
+@AllArgsConstructor
 public class ChauffeurController {
+    private final ChauffeurService chauffeurService;
+    @GetMapping
+    public List<ChauffeurDTO> listChauffeuresDispo(){
+        return chauffeurService.cheuffeursDisponibles();
+    }
+
+    @PostMapping("/ajouter")
+        public ChauffeurDTO ajouterChauffeur(@RequestBody ChauffeurDTO chauffeurDTO){
+        return chauffeurService.ajouterChauffeur(chauffeurDTO);
+    }
+
+    @DeleteMapping("/supprimer/{id}")
+    public void supprimerChauffeur(@PathVariable long id){
+        chauffeurService.supprimerChauffeur(id);
+    }
+
+  @PostMapping("/modifier/{id}")
+    public ChauffeurDTO modifierChauffeur(@PathVariable long id, @RequestBody ChauffeurDTO chauffeurDTO){
+       return chauffeurService.modifierChauffeur(id,chauffeurDTO);
+  }
+
+
 }
--- src/main/java/org/example/fleetflow/service/VehiculeService.java
+++ src/main/java/org/example/fleetflow/service/VehiculeService.java
@@ -1,23 +1,23 @@
-package org.example.fleetflow.service;
-
-import org.example.fleetflow.DTO.VehiculeDTO;
-import org.example.fleetflow.Repository.VehiculeRepository;
-import org.example.fleetflow.model.Vehicule;
-import org.springframework.stereotype.Service;
-
-import java.util.stream.Collectors;
-
-@Service
-public class VehiculeService {
-    private final VehiculeRepository vehiculeRepository;
-    private final VehiculeMapper vehiculeMapper;
-    public VehiculeDTO createVehicule(vehiculeDTO dto){
-        Vehicule vehicule = vehiculeMapper.toEntity(dto);
-        Vehicule saved = vehiculeRepository.save(vehicule);
-        return vehiculeMapper.toDto(saved);
-    }
-    public List<VehiculeDTO> getVehiculesDisponible(){
-        return vehiculeRepository.findByStatut(Vehicule.StatutVehicule.DISPONIBLE)
-                .stream().map(vehiculeMapper::toDto).collect(Collectors.toList());
-    }
-}
+//package org.example.fleetflow.service;
+//
+//import org.example.fleetflow.DTO.VehiculeDTO;
+//import org.example.fleetflow.Repository.VehiculeRepository;
+//import org.example.fleetflow.model.Vehicule;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.stream.Collectors;
+//
+//@Service
+//public class VehiculeService {
+//    private final VehiculeRepository vehiculeRepository;
+//    private final VehiculeMapper vehiculeMapper;
+//    public VehiculeDTO createVehicule(vehiculeDTO dto){
+//        Vehicule vehicule = vehiculeMapper.toEntity(dto);
+//        Vehicule saved = vehiculeRepository.save(vehicule);
+//        return vehiculeMapper.toDto(saved);
+//    }
+//    public List<VehiculeDTO> getVehiculesDisponible(){
+//        return vehiculeRepository.findByStatut(Vehicule.StatutVehicule.DISPONIBLE)
+//                .stream().map(vehiculeMapper::toDto).collect(Collectors.toList());
+//    }
+//}
