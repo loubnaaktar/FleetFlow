396ad83dd09da670dfee6364879315955b318738
noha20009 <machkourinouhayla@gmail.com>
2026-04-02 17:16:58 +0100

    controller livraison

--- src/main/java/org/example/fleetflow/controller/LivraisonController.java
+++ src/main/java/org/example/fleetflow/controller/LivraisonController.java
@@ -1,13 +1,49 @@
 package org.example.fleetflow.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.fleetflow.DTO.LivraisonDTO;
+import org.example.fleetflow.DTO.VehiculeDTO;
+import org.example.fleetflow.model.Livraison;
 import org.example.fleetflow.service.LivraisonService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/livraison")
 @RequiredArgsConstructor
 public class LivraisonController {
+
     private final LivraisonService livraisonService;
+
+    @PostMapping
+    public LivraisonDTO create(@RequestParam long idClient) {
+        return livraisonService.ajouterLivraison(idClient);
+    }
+
+    @PutMapping("/{id}/assigner")
+    public LivraisonDTO assigner(
+            @PathVariable long id,
+            @RequestParam long idChauffeur,
+            @RequestParam long idVehicule){
+
+        return livraisonService.assignerChauffeurVehicule(id,idChauffeur,idVehicule);
+    }
+
+    @GetMapping
+    public List<LivraisonDTO> getAll(){
+        return livraisonService.listLivraisons();
+    }
+
+    @PutMapping("/{id}/statut")
+    public LivraisonDTO updateStatut(
+            @PathVariable long id,
+            @RequestParam Livraison.StatutLivraison statut){
+        return livraisonService.modifierStatutLivraison(id, statut);
+    }
 }
+
+
+
+
+
