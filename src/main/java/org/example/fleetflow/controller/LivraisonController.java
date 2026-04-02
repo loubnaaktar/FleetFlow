cc16ee51736170bd3137c4041ea9686da0bfaa78
noha20009 <machkourinouhayla@gmail.com>
2026-04-02 16:10:24 +0100

    controller

--- src/main/java/org/example/fleetflow/controller/LivraisonController.java
+++ src/main/java/org/example/fleetflow/controller/LivraisonController.java
@@ -1,4 +1,13 @@
 package org.example.fleetflow.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.fleetflow.service.LivraisonService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/livraison")
+@RequiredArgsConstructor
 public class LivraisonController {
+    private final LivraisonService livraisonService;
 }
