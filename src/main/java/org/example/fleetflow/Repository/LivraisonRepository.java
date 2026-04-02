8ca5ea95123de2769c09a22a6304e978179880ff
noura.erraqioui <noura.erraqioui@gmail.com>
2026-04-02 16:17:55 +0100

    save

--- src/main/java/org/example/fleetflow/Repository/LivraisonRepository.java
+++ src/main/java/org/example/fleetflow/Repository/LivraisonRepository.java
@@ -1,9 +1,16 @@
 package org.example.fleetflow.Repository;
 
+import org.example.fleetflow.model.Client;
 import org.example.fleetflow.model.Livraison;
+import org.example.fleetflow.model.Vehicule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LivraisonRepository extends JpaRepository<Livraison, Long> {
+    List<Livraison> findByStatut(String statut);
+   List<Livraison> findByClientId(Long id);
+
 }
