23b1167306c1d7d67c5bbd37d1f74046f83beb00
noura.erraqioui <noura.erraqioui@gmail.com>
2026-04-03 09:54:25 +0100

    REPLIVRAISON

--- src/main/java/org/example/fleetflow/Repository/LivraisonRepository.java
+++ src/main/java/org/example/fleetflow/Repository/LivraisonRepository.java
@@ -4,13 +4,19 @@ import org.example.fleetflow.model.Client;
 import org.example.fleetflow.model.Livraison;
 import org.example.fleetflow.model.Vehicule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface LivraisonRepository extends JpaRepository<Livraison, Long> {
     List<Livraison> findByStatut(String statut);
    List<Livraison> findByClientId(Long id);
+   @Query("select l from Livraison l where l.dateLivraison BETWEEN :date1 AND :date2")
+   List<Livraison> findLivraisonBetweenTwoDate(LocalDateTime date1,LocalDateTime date2);
+   @Query("select l from Livraison l where l.adresseDestination = :ville")
+   List<Livraison> findLivraisonByVille(String ville);
 
 }
